380d41b67ebf033380fc9cf8cb68ca55b0077d5a
djahern-max <danielaherniv@gmail.com>
2021-02-13 19:24:12 -0500

    update

--- app.js
+++ app.js
@@ -1,81 +1,98 @@
 //jshint esversion:6
 
-require('dotenv').config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+// const encrypt = require('mongoose-encryption')
+// const md5 = require('md5')
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10
 
+const app = express()
 
-const app = express();
+// console.log(md5('123456'))
 
-console.log(process.env.API_KEY)
+app.use(express.static('public'))
 
-app.use(express.static("public"));
+app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
 })
 
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ['password'],
+// })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+const User = new mongoose.model('User', userSchema)
 
-const User = new mongoose.model("User", userSchema)
-
-app.get("/", function(req, res){
-  res.render("home");
+app.get('/', function (req, res) {
+  res.render('home')
 })
 
-app.get("/login", function(req, res){
-  res.render("login");
+app.get('/login', function (req, res) {
+  res.render('login')
 })
 
-app.get("/register", function(req, res){
-  res.render("register");
+app.get('/register', function (req, res) {
+  res.render('register')
 })
 
-app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
+// app.post('/register', function (req, res) {
+//   bcrypt.genSalt(saltRounds, function (err, salt) {
+//     bcrypt.hash(req.body.password, salt, function (err, hash) {
+//       const newUser = new User({
+//         email: req.body.username,
+//         // password: req.body.password,
+//         // password: md5(req.body.password),
+//         password: hash,
+//       })
 
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  })
+//       newUser.save(function (err) {
+//         if (err) {
+//           console.log(err)
+//         } else {
+//           res.render('secrets')
+//         }
+//       })
+//     })
+//   })
+// })
+
+app.post('/register', function (req, res) {})
+
+// app.post('/login', function (req, res) {
+//   const username = req.body.username
+//   const password = req.body.password
+
+//   User.findOne({ email: username }, function (err, foundUser) {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       if (foundUser) {
+//         bcrypt.compare(password, foundUser.password, function (err, result) {
+//           if (result === true) {
+//             res.render('secrets')
+//           }
+//         })
+//       }
+//     }
+//   })
+// })
+
+app.post('/login', function (req, res) {})
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000')
 })
-
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password =  req.body.password;
-
-  User.findOne({email: username}, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
-    }
-  })
-})
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
